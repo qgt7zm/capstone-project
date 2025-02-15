caa1f2ba04c4ee93c3cf4852ff22d1f1464fb6a1
qgt7zm <qgt7zm@virginia.edu>
2025-02-15 17:11:08 -0500

    Create placeholder homepage view

---
+++ myapp/urls.py
@@ -0,0 +1,13 @@
+"""
+URL configuration for myapp application.
+"""
+
+from django.urls import path
+
+from . import views
+
+app_name = "myapp"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("home", views.home, name="home"),
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+"""
+Views for myapp application.
+"""
 
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+
+def index(request) -> HttpResponse:
+    return HttpResponseRedirect(reverse("myapp:home"))
+
+def home(request):
+    return HttpResponse("You've reached the homepage.")
