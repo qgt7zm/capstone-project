88522d39e5064c51f8166777d433416de37f1d1c
qgt7zm <qgt7zm@virginia.edu>
2025-03-18 17:00:10 -0400

    Save form fields after search

--- myapp/views.py
+++ myapp/views.py
@@ -25,39 +25,46 @@ def home(request) -> HttpResponse:
 
 
 def elements(request) -> HttpResponse:
-    filtered_results = Element.objects.all()
+    filtered_elements = Element.objects.all()
+    form_fields = {}
 
     if request.method == "GET":
         action = request.GET.get("action", None)
         if action == "search":
             # Filter results using form
             any_filter = request.GET.get("any", None)
+            form_fields["any"] = any_filter
             if any_filter:
-                filtered_results = (
-                        filtered_results.filter(name__icontains=any_filter) |
-                        filtered_results.filter(description__icontains=any_filter) |
-                        filter_by_outcome_name(filtered_results, any_filter) |
-                        filter_by_resource_citation(filtered_results, any_filter, "outcomes")
+                filtered_elements = (
+                        filtered_elements.filter(name__icontains=any_filter) |
+                        filtered_elements.filter(description__icontains=any_filter) |
+                        filter_by_outcome_name(filtered_elements, any_filter) |
+                        filter_by_resource_citation(filtered_elements, any_filter, "outcomes")
                 )
 
             name_filter = request.GET.get("name", None)
+            form_fields["name"] = name_filter
             if name_filter:
-                filtered_results = filtered_results.filter(name__icontains=name_filter)
+                filtered_elements = filtered_elements.filter(name__icontains=name_filter)
 
             description_filter = request.GET.get("description", None)
+            form_fields["description"] = description_filter
             if description_filter:
-                filtered_results = filtered_results.filter(description__icontains=description_filter)
+                filtered_elements = filtered_elements.filter(description__icontains=description_filter)
 
             outcome_filter = request.GET.get("outcome", None)
+            form_fields["outcome"] = outcome_filter
             if outcome_filter:
-                filtered_results = filter_by_outcome_name(filtered_results, outcome_filter)
+                filtered_elements = filter_by_outcome_name(filtered_elements, outcome_filter)
 
             resource_filter = request.GET.get("resource", None)
+            form_fields["resource"] = resource_filter
             if resource_filter:
-                filtered_results = filter_by_resource_citation(filtered_results, resource_filter, "outcomes")
+                filtered_elements = filter_by_resource_citation(filtered_elements, resource_filter, "outcomes")
 
     context = {
-        "elements": filtered_results,
+        "elements": filtered_elements,
+        "form_fields": form_fields,
     }
     return render(
         request,
@@ -67,39 +74,46 @@ def elements(request) -> HttpResponse:
 
 
 def outcomes(request) -> HttpResponse:
-    filtered_results = Outcome.objects.all()
+    filtered_outcomes = Outcome.objects.all()
+    form_fields = {}
 
     if request.method == "GET":
         action = request.GET.get("action", None)
         if action == "search":
             # Filter results using form
             any_filter = request.GET.get("any", None)
+            form_fields["any"] = any_filter
             if any_filter:
-                filtered_results = (
-                        filtered_results.filter(name__icontains=any_filter) |
-                        filtered_results.filter(description__icontains=any_filter) |
-                        filter_by_element_name(filtered_results, any_filter) |
-                        filter_by_resource_citation(filtered_results, any_filter, "outcomes")
+                filtered_outcomes = (
+                        filtered_outcomes.filter(name__icontains=any_filter) |
+                        filtered_outcomes.filter(description__icontains=any_filter) |
+                        filter_by_element_name(filtered_outcomes, any_filter) |
+                        filter_by_resource_citation(filtered_outcomes, any_filter, "outcomes")
                 )
 
             name_filter = request.GET.get("name", None)
+            form_fields["name"] = name_filter
             if name_filter:
-                filtered_results = filtered_results.filter(name__icontains=name_filter)
+                filtered_outcomes = filtered_outcomes.filter(name__icontains=name_filter)
 
             description_filter = request.GET.get("description", None)
+            form_fields["description"] = description_filter
             if description_filter:
-                filtered_results = filtered_results.filter(description__icontains=description_filter)
+                filtered_outcomes = filtered_outcomes.filter(description__icontains=description_filter)
 
             element_filter = request.GET.get("element", None)
+            form_fields["element"] = element_filter
             if element_filter:
-                filtered_results = filter_by_element_name(filtered_results, element_filter)
+                filtered_outcomes = filter_by_element_name(filtered_outcomes, element_filter)
 
             resource_filter = request.GET.get("resource", None)
+            form_fields["resource"] = resource_filter
             if resource_filter:
-                filtered_results = filter_by_resource_citation(filtered_results, resource_filter, "outcomes")
+                filtered_outcomes = filter_by_resource_citation(filtered_outcomes, resource_filter, "outcomes")
 
     context = {
-        "outcomes": filtered_results,
+        "outcomes": filtered_outcomes,
+        "form_fields": form_fields,
     }
     return render(
         request,
@@ -109,39 +123,46 @@ def outcomes(request) -> HttpResponse:
 
 
 def resources(request) -> HttpResponse:
-    filtered_results = order_by_citation(Resource.objects.all())
+    filtered_resources = order_by_citation(Resource.objects.all())
+    form_fields = {}
 
     # Filter results using form
     if request.method == "GET":
         action = request.GET.get("action", None)
         if action == "search":
             any_filter = request.GET.get("any", None)
+            form_fields["any"] = any_filter
             if any_filter:
-                filtered_results = (
-                        filtered_results.filter(title__icontains=any_filter) |
-                        filter_by_author_name(filtered_results, any_filter) |
-                        filter_by_year_str(filtered_results, any_filter) |
-                        filtered_results.filter(summary__icontains=any_filter)
+                filtered_resources = (
+                        filtered_resources.filter(title__icontains=any_filter) |
+                        filter_by_author_name(filtered_resources, any_filter) |
+                        filter_by_year_str(filtered_resources, any_filter) |
+                        filtered_resources.filter(summary__icontains=any_filter)
                 ).distinct()
 
-            summary_filter = request.GET.get("summary", None)
-            if summary_filter:
-                filtered_results = filtered_results.filter(summary__icontains=summary_filter)
+            title_filter = request.GET.get("title", None)
+            form_fields["title"] = title_filter
+            if title_filter:
+                filtered_resources = filtered_resources.filter(title__icontains=title_filter)
 
-            name_filter = request.GET.get("name", None)
-            if name_filter:
-                filtered_results = filter_by_author_name(filtered_results, any_filter)
+            author_filter = request.GET.get("author", None)
+            form_fields["author"] = author_filter
+            if author_filter:
+                filtered_resources = filter_by_author_name(filtered_resources, author_filter).distinct()
 
-            year = request.GET.get("year", None)
-            if year:
-                filtered_results = filtered_results.filter(year=year)
+            year_filter = request.GET.get("year", None)
+            form_fields["year"] = year_filter
+            if year_filter:
+                filtered_resources = filtered_resources.filter(year=year_filter)
 
-            summary_filter = request.GET.get("summary", None)
-            if summary_filter:
-                filtered_results = filtered_results.filter(summary__icontains=summary_filter)
+            title_filter = request.GET.get("summary", None)
+            form_fields["summary"] = title_filter
+            if title_filter:
+                filtered_resources = filtered_resources.filter(summary__icontains=title_filter)
 
     context = {
-        "resources": filtered_results
+        "resources": filtered_resources,
+        "form_fields": form_fields,
     }
     return render(
         request,
@@ -272,47 +293,54 @@ def add_result_form(request, resource_pk: int) -> HttpResponse:
 
 
 def scenarios(request) -> HttpResponse:
-    filtered_results = Scenario.objects.all()
+    filtered_scenarios = Scenario.objects.all()
+    form_fields = {}
 
     if request.method == "GET":
         action = request.GET.get("action", None)
         if action == "search":
             # Filter results using form
             any_filter = request.GET.get("any", None)
+            form_fields["any"] = any_filter
             if any_filter:
-                filtered_results = (
-                        filtered_results.filter(name__icontains=any_filter) |
-                        filtered_results.filter(
+                filtered_scenarios = (
+                        filtered_scenarios.filter(name__icontains=any_filter) |
+                        filtered_scenarios.filter(
                             subject=
                             get_choice_from_label_icontains(Subjects, any_filter)
                         ) |
-                        filtered_results.filter(
+                        filtered_scenarios.filter(
                             age_group=
                             get_choice_from_label_icontains(AgeGroups, any_filter)
                         ) |
-                        filtered_results.filter(outcomes__name__icontains=any_filter)
+                        filtered_scenarios.filter(outcomes__name__icontains=any_filter)
                 ).distinct()
 
             name_filter = request.GET.get("name", None)
+            form_fields["name"] = name_filter
             if name_filter:
-                filtered_results = filtered_results.filter(name__icontains=name_filter)
+                filtered_scenarios = filtered_scenarios.filter(name__icontains=name_filter)
 
             subject_filter = request.GET.get("subject", None)
+            form_fields["subject"] = subject_filter
             if subject_filter:
                 subject_filter = get_choice_from_label(Subjects, subject_filter)
-                filtered_results = filtered_results.filter(subject=subject_filter)
+                filtered_scenarios = filtered_scenarios.filter(subject=subject_filter)
 
             age_group_filter = request.GET.get("age_group", None)
+            form_fields["age_group"] = age_group_filter
             if age_group_filter:
                 age_group_filter = get_choice_from_label(AgeGroups, age_group_filter)
-                filtered_results = filtered_results.filter(age_group=age_group_filter)
+                filtered_scenarios = filtered_scenarios.filter(age_group=age_group_filter)
 
             outcome_filter = request.GET.get("outcome", None)
+            form_fields["outcome"] = outcome_filter
             if outcome_filter:
-                filtered_results = filtered_results.filter(outcomes__name__icontains=outcome_filter)
+                filtered_scenarios = filtered_scenarios.filter(outcomes__name__icontains=outcome_filter)
 
     context = {
-        "scenarios": filtered_results,
+        "scenarios": filtered_scenarios,
+        "form_fields": form_fields,
         "subject_choices": Subjects.labels,
         "age_group_choices": AgeGroups.labels,
     }
