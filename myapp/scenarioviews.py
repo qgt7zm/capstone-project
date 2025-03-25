0e25c5038aaf674f1efa8745893cfdea259a77ee
qgt7zm <qgt7zm@virginia.edu>
2025-03-24 20:04:35 -0400

    Move scenario views to separate file

---
+++ myapp/scenarioviews.py
@@ -0,0 +1,98 @@
+"""
+Scenario views for myapp application.
+"""
+from django.contrib import messages
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+
+from myapp.models import Scenario, Subjects, AgeGroups, Outcome
+from myapp.modelutils import get_choice_from_label
+from myapp.recommender import get_recommendations_combined
+from myapp.viewutils import filter_scenarios
+
+
+def scenarios(request) -> HttpResponse:
+    filtered_scenarios = Scenario.objects.all()
+    form_fields = {}
+
+    if request.method == "GET":
+        action = request.GET.get("action", None)
+        if action == "search":
+            filtered_scenarios = filter_scenarios(
+                request, filtered_scenarios, form_fields
+            )
+
+    context = {
+        "scenarios": filtered_scenarios,
+        "form_fields": form_fields,
+        "subject_choices": Subjects.labels,
+        "age_group_choices": AgeGroups.labels,
+    }
+    return render(
+        request,
+        "myapp/scenarios.html",
+        context
+    )
+
+
+def scenario_view(request, scenario_pk: int) -> HttpResponse:
+    scenario = get_object_or_404(Scenario, pk=scenario_pk)
+    recommendations = get_recommendations_combined(scenario)
+
+    context = {
+        "scenario": scenario,
+        "recommendations": recommendations,
+    }
+    return render(
+        request,
+        "myapp/scenario_view.html",
+        context
+    )
+
+
+def add_scenario(request) -> HttpResponse:
+    context = {
+        "outcome_choices": [outcome.name for outcome in Outcome.objects.all()],
+        "subject_choices": Subjects.labels,
+        "age_group_choices": AgeGroups.labels,
+    }
+
+    return render(
+        request,
+        "myapp/add_scenario.html",
+        context
+    )
+
+
+def add_scenario_form(request) -> HttpResponse:
+    if request.method == "POST":
+        action = request.POST.get("action", None)
+        if action == "add_scenario":
+            name = request.POST.get("name")
+            existing_scenario = Scenario.objects.filter(name=name)
+
+            new_name = name
+            counter = 1
+            while existing_scenario.exists():
+                # Scenario already exists
+                new_name = f"{name} ({counter})"
+                existing_scenario = Scenario.objects.filter(name=new_name)
+                counter += 1
+
+            outcome_names = request.POST.getlist("outcomes")
+            scenario_outcomes = Outcome.objects.filter(name__in=outcome_names)
+
+            subject_label = request.POST.get("subject", None)
+            subject = get_choice_from_label(Subjects, subject_label)
+            age_group_label = request.POST.get("age_group", None)
+            age_group = get_choice_from_label(AgeGroups, age_group_label)
+
+            # Create Scenario
+            scenario = Scenario.objects.create(
+                name=new_name, subject=subject, age_group=age_group
+            )
+            scenario.outcomes.set(scenario_outcomes)
+
+            messages.success(request, f'Scenario "{new_name}" created successfully.')
+
+    return redirect("myapp:scenarios")
--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,7 @@ URL configuration for myapp application.
 
 from django.urls import path
 
-from . import views
-from . import resourceviews
+from . import resourceviews, scenarioviews, views
 
 app_name = "myapp"
 urlpatterns = [
@@ -19,10 +18,10 @@ urlpatterns = [
     path("resources/add/form", resourceviews.add_resource_form, name="add_resource_form"),
     path("resources/<int:resource_pk>/add_result", views.add_result, name="add_result"),
     path("resources/<int:resource_pk>/add_result/form", views.add_result_form, name="add_result_form"),
-    path("scenarios", views.scenarios, name="scenarios"),
-    path("scenarios/<int:scenario_pk>", views.scenario_view, name="scenario_view"),
-    path("scenarios/add", views.add_scenario, name="add_scenario"),
-    path("scenarios/add/form", views.add_scenario_form, name="add_scenario_form"),
+    path("scenarios", scenarioviews.scenarios, name="scenarios"),
+    path("scenarios/<int:scenario_pk>", scenarioviews.scenario_view, name="scenario_view"),
+    path("scenarios/add", scenarioviews.add_scenario, name="add_scenario"),
+    path("scenarios/add/form", scenarioviews.add_scenario_form, name="add_scenario_form"),
     path("data", views.data, name="data"),
     path("data/export", views.data_export, name="data_export"),
     path("data/upload", views.data_upload, name="data_upload"),
